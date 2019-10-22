696dfb005d68c446aae6bd7b97bd32e09994647f
Patience Deaton <46998183+PatienceD@users.noreply.github.com>
2019-10-22 13:41:04 -0400

    added to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,13 +15,14 @@ class Navbar extends Component {
                     </div>
                 </nav>
                 <nav className="navbar navbar-expand-lg navbar-black ">
-                    <img src={logo} style={{ height: '80px', margin: '15px' }}></img>
-                    <a className="navbar-brand" href="#">Learn Sativa</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
+                    <img src={logo} style={{ height: '80px', margin: '15px' }}></img>
+                    <a className="navbar-brand" href="#">Learn Sativa</a>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
+
                             <li className="nav-item active">
                                 <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                             </li>
